174c1e9c7cd79a1b33ac6f4f2ff75ae2460b91be
Dell <Dell@Dell-PC>
2020-10-01 21:47:37 +0530

    assignment

--- ProViligeResource/src/main/java/com/provigil/cost/resource/ProvigilCostCalculatorApplication.java
+++ ProViligeResource/src/main/java/com/provigil/cost/resource/ProvigilCostCalculatorApplication.java
@@ -2,7 +2,7 @@ package com.provigil.cost.resource;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
-@ApplicationPath("/resources")
+@ApplicationPath("/resource")
 public class ProvigilCostCalculatorApplication extends Application{
 
 }
